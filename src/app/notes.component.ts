1c199d7741f2f05b240847b417aa292dbe037de4
DSK5 DSK <email@example.com>
2018-11-15 12:24:23 +0200

    T2. 2. Show notes in NotesComponent

--- src/app/notes.component.ts
+++ src/app/notes.component.ts
@@ -2,6 +2,20 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'notes',
-  template: `Notes list:`
+  template: `Notes list:
+    <ul>
+      <li *ngFor="let note of notes ">
+        {{note.text}}
+      </li>
+    </ul>`
 })
-export class NotesComponent { }
+export class NotesComponent {
+  notes: Note[] = [
+    {text: 'Note one'},
+    {text: 'Note two'}
+  ];
+}
+
+interface Note {
+  text: string;
+}
