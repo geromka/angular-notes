0f4ee63d35883bf4604aa49aeffe138bf59e9299
DSK5 DSK <email@example.com>
2018-11-15 11:51:50 +0200

    T2. 1. Create and use NotesComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Notes Angular 2 App</h1>
+    <notes></notes>`
 })
 export class AppComponent {
   title = 'angular-notes';
